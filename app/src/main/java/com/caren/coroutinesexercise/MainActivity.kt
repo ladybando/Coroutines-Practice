703f05f2f42c9f937e208845ef5294ba8c0d2b30
Caren Chang <carenc@google.com>
2021-10-11 12:05:55 -0700

    Change from toast to counter button

--- app/src/main/java/com/caren/coroutinesexercise/MainActivity.kt
+++ app/src/main/java/com/caren/coroutinesexercise/MainActivity.kt
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var num = 0
+
         findViewById<Button>(R.id.button).setOnClickListener {
-            Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
+            num++
+            findViewById<TextView>(R.id.textView).text = num.toString()
         }
 
         findViewById<Button>(R.id.button2).setOnClickListener {
